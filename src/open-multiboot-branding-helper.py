9e55f067737daa05bd0792b22dd5756e5ddec1f7
jack2015 <344784320@qq.com>
2021-11-25 04:46:11 +0800

    update

--- src/open-multiboot-branding-helper.py
+++ src/open-multiboot-branding-helper.py
@@ -58,7 +58,7 @@ else:
 			for key in list(KEYS_FNC_MAP.keys()):
 				print(key + ' = ' + eval(KEYS_FNC_MAP[key]))
 		else:
-			if WORKAROUND or (sys.argv[2] == 'image_distro' or sys.argv[2] == 'image_version'):
+			if WORKAROUND or (sys.argv[2] == 'image_distro' or sys.argv[2] == 'image_version' or sys.argv[2] == 'brand_oem' or sys.argv[2] == 'box_type'):
 				if sys.argv[2] == 'image_distro':
 					try:
 						print(open(sys.argv[1].replace("/usr/lib/enigma2/python","")+"/etc/issue").readlines()[-2].capitalize().strip()[:-6])

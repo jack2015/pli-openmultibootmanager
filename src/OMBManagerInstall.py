055071a61a6499227cd793c25420065912a15caf
jackgee2021 <344784320@qq.com>
2025-11-12 10:24:43 +0000

    update src/OMBManagerInstall.py.
    Signed-off-by: jackgee2021 <344784320@qq.com>

--- src/OMBManagerInstall.py
+++ src/OMBManagerInstall.py
@@ -774,7 +774,7 @@ class OMBManagerInstall(Screen):
 		else:
 			return False
 
-		for pyver in [ "2.7", "3.8", "3.9", "3.10"]:
+		for pyver in [ "2.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13"]:
 			if os.path.exists('/usr/lib/python' + pyver):
 				if not os.path.exists('/usr/lib/python' + pyver +'/boxbranding.so') and os.path.exists('/usr/lib/enigma2/python/boxbranding.so'):
 					os.system('ln -sf /usr/lib/enigma2/python/boxbranding.so /usr/lib/python' + pyver +'/boxbranding.so')
